1d767afa07a6ef62594e866c92f3ce7fb454504c
Bisera Milosheska <bisera.milosheska@cognite.com>
2022-03-29 15:43:51 +0200

    Configure .gitignore, .eslint

--- index.js
+++ index.js
@@ -1,26 +1,41 @@
 const core = require('@actions/core');
-const { exec } = require('child_process');
+const {exec} = require('child_process');
 
-async function sign(certificate, certificate_password, path_to_binary, recursive=false) {
-    core.info(`Execute PS script to sign binary`);
+/** Sign binary
+ * @param {string} certificate - The base64 encoded certificate.
+ * @param {string} certificatePassword - The password for the certificate.
+ * @param {string} pathToBinary - The folder that contains the files to sign.
+ * @param {string} recursive - Recursively search for supported files.
+*/
+async function sign(certificate,
+    certificatePassword,
+    pathToBinary,
+    recursive=false) {
+  core.info(`Execute PS script to sign binary`);
 
-    exec("./sign.ps1", [certificate, certificate_password, path_to_binary, recursive], (error, stdout, stderr) => {
+  exec('./sign.ps1',
+      [certificate, certificatePassword, pathToBinary, recursive],
+      (error, stdout, stderr) => {
         if (error) {
           console.error(`exec error: ${error}`);
-          core.setFailed(err.message);
+          core.setFailed(error.message);
         }
         console.log(`stdout: ${stdout}`);
         console.error(`stderr: ${stderr}`);
       });
 }
+
 async function run() {
   try {
     const certificate = core.getInput('certificate');
-    const certificate_password = core.getInput('certificate-password');
-    const path_to_binary = core.getInput('path-to-binary');
+    const certificatePassword = core.getInput('certificate-password');
+    const pathToBinary = core.getInput('path-to-binary');
     const recursive = core.getInput('recursive');
-    
-    const { output } = await sign(certificate, certificate_password, path_to_binary, recursive);
+
+    const {output} = await sign(certificate,
+        certificatePassword,
+        pathToBinary,
+        recursive);
 
     core.setOutput('result', output);
   } catch (error) {
