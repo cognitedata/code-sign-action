f60e3f9668c7f7c57d0cc3381ed7667459e1ed2b
Einar <einar@omang.com>
2023-04-19 11:54:08 +0200

    Make action work on linux (#6)
    Use osslsigncode to sign binaries on linux as well.
    
    This was slightly painful. The version of openssl installed by default requires specifying the "legacy" flag to work with RC2 certificates, the easiest way I found was to pass the certificate through openssl. This depends on ubuntu version as well, openssl 1.x on 20 works, 3.x on 22 requires this workaround.

---
+++ sign.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+set -e
+
+echo -n "$CERTIFICATE" | base64 -w 0 --decode > ./cognite_code_signing.pfx
+
+
+osslver="$(openssl version)"
+if [[ "$osslver" == "OpenSSL 3"* ]]; then
+    # Convert the pkcs12 file into a compatible format by passing it through openssl...
+    # Technically the sign tool is also based on openssl, but it doesn't let you set the "legacy" flag,
+    # so we have to convert it using openssl first.
+    # Note that this is dependent on the version of openssl, and the type of certificate.
+    # This works for the cognite code certificate
+    openssl pkcs12 -in ./cognite_code_signing.pfx -out ./cognite_code_signing.pem -legacy \
+        -passin "pass:$CERTIFICATE_PASSWORD" -passout "pass:$CERTIFICATE_PASSWORD"
+    openssl pkcs12 -in ./cognite_code_signing.pem \
+        -keypbe PBE-SHA1-3DES -certpbe PBE-SHA1-3DES \
+        -export -out ./cognite_code_signing_2.pfx \
+        -passin "pass:$CERTIFICATE_PASSWORD" -passout "pass:$CERTIFICATE_PASSWORD"
+    mv ./cognite_code_signing_2.pfx ./cognite_code_signing.pfx
+    rm ./cognite_code_signing.pem
+fi
+
+recurse=false
+file_path="$1"
+
+for var in "$@"; do
+    if [ "$var" = "-Recurse" ] ; then
+        recurse=true
+    fi
+done
+
+sign_binary() {
+    osslsigncode sign -pkcs12 ./cognite_code_signing.pfx -pass "$CERTIFICATE_PASSWORD" \
+        -t "http://timestamp.digicert.com" \
+        -in "$1" -out "$1.signed"
+    mv "$1.signed" "$1"
+}
+
+if [ $recurse = true ] ; then
+    echo "Sign all files in folder $file_path"
+    for f in $(find $file_path -type f); do
+        sign_binary $f
+    done
+else
+    echo "Sign a single binary"
+    sign_binary $file_path
+fi
+
+rm ./cognite_code_signing.pfx
